4578895da34b97a65ed52e9fe2d2c37c872e9b61
marzonha <marzonhapacheco@gmail>
2024-05-24 17:03:26 -0300

    .git Passei ele para fora da pasta, para englobar os modules

---
+++ vite-project/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+
+export function Header() {
+  return (
+    <header>
+        <h1>Página Header</h1>
+    </header>
+
+  )
+}
---
+++ vite-project/src/Components/Main.jsx
@@ -0,0 +1,9 @@
+
+export  function Main() {
+  return (
+    <main>
+        <h1>Página Main</h1>
+    </main>
+
+  )
+}
